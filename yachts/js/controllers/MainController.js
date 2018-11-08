a054db2a40e6f5bcf4d5432e053714c4ecf2126a
Instructor <jsitbon@qcc.cuny.edu>
2018-11-08 09:59:54 -0500

    end of class

--- yachts/js/controllers/MainController.js
+++ yachts/js/controllers/MainController.js
@@ -1,6 +1,34 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'This Month\'s Bestsellers';
   $scope.promo = 'The most popular books this month.';
-  $scope.products = [ { name: 'The Book of Trees', price: 19, pubdate: new Date('2014', '03', '08'), cover: 'img/the-book-of-trees.jpg' },
-  { name: 'Program or be Programmed', price: 8, pubdate: new Date('2013', '08', '01'), cover: 'img/program-or-be-programmed.jpg' } ]
+  $scope.products = [
+  	{
+    	name: 'The Book of Trees',
+    	price: 19,
+      likes: 0,
+    	pubdate: new Date('2014', '03', '08'),
+    	cover: 'img/the-book-of-trees.jpg'
+  	},
+  	{
+    	name: 'Program or be Programmed',
+    	price: 8,
+      likes: 0,
+    	pubdate: new Date('2013', '08', '01'),
+    	cover: 'img/program-or-be-programmed.jpg'
+  	},
+  	{
+    	name: 'Harry Potter & The Prisoner of Azkaban',
+    	price: 11.99,
+      likes: 0,
+    	pubdate: new Date('1999', '07', '08'),
+    	cover: 'http://upload.wikimedia.org/wikipedia/en/b/b4/Harry_Potter_and_the_Prisoner_of_Azkaban_(US_cover).jpg'
+  	},
+  	{
+    	name: 'Ready Player One',
+    	price: 7.99,
+      likes: 0,
+    	pubdate: new Date('2011', '08', '16'),
+    	cover: 'http://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg'
+  	}
+  ]
 }]);
